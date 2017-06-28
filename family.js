92ed8acb3932eed8c18c0fde5619cc4f6ad9256d
PA <palbcn@yahoo.com>
2017-06-28 12:35:52 +0200

    familia arreglada

--- family.js
+++ family.js
@@ -1,5 +1,6 @@
-var family = [ 
+var family = [
     { id: 'iSebas', name: 'Sebas (1918)',
+	    gen: 'root',
       spouse: { id: 'iDusia', name: 'Dusia' },
       children: [ 
         { id: 'iSebuchi',
@@ -27,14 +28,16 @@ var family = [
           ]}
       ]},
     { id: 'iAntonio', name: 'Antonio (1923)',
+	  gen: 'root',
       spouse: { id: 'iJoaquina', name: 'Joaquina' },
       children: [
         { id: 'iTonyin', name: 'Toñin',
           spouse: { id: 'iTrini', name: 'Trini' },
           children: [
-            { id: 'iSergio', name: 'Sergio' },
-            { id: 'iMaite', name: 'Maite' },
-            { id: 'iViena', name: 'Viena' }
+            { id: 'iSergio', name: 'Sergio' ,
+              spouse: { id: 'iMaite', name: 'Maite' },
+              children: [ { id: 'iViena', name: 'Viena' } ]
+			}
           ]
         },
         { id: 'iMaripau',
@@ -46,12 +49,14 @@ var family = [
             img: 'http://usuaris.tinet.cat/sag/imatges/altree/juan_maripau.png',
             name: 'Juan' },
           children: [
-            { id: 'iSandra', name: 'Sandra' },
-            { id: 'iOriolSandra', name: 'Oriol' } 
+            { id: 'iSandra', name: 'Sandra',
+              spouse: { id: 'iOriolSandra', name: 'Oriol' } 
+			}
           ]
-         }
+        }
     ]},
     { id: 'iJoseMaria', name: 'Jose Maria (1927)',
+	  gen: 'root',
       spouse: { id: 'iJosefina', name: 'Josefina' },
       children: [
         { id: 'iMariaJose',
@@ -79,187 +84,210 @@ var family = [
             { id: 'iJorgeMo', name: 'Jorge' } 
           ]
         } 
-    ]
-  },
-  { id: 'iJacinto', name: 'Jacinto (1930)',
-    spouse: { 
-      id: 'iConchita',
+    ]},
+    { id: 'iJacinto', name: 'Jacinto (1930)',
       gen: 'root',
-      img: 'http://usuaris.tinet.cat/sag/imatges/altree/conchita.png',
-      name: 'Conchita' },
-    children: [ 
-    { id: 'iHerminiaJa',
-      gen: 'fills',
-      img: 'http://usuaris.tinet.cat/sag/imatges/altree/herminia.png',
-      name: 'Herminia',
       spouse: { 
-        id: 'iManolo',
+        id: 'iConchita',
+        gen: 'root',
+        img: 'http://usuaris.tinet.cat/sag/imatges/altree/conchita.png',
+        name: 'Conchita' },
+      children: [ 
+      { id: 'iHerminiaJa',
         gen: 'fills',
-        img: 'http://usuaris.tinet.cat/sag/imatges/altree/manolo.png',
-        name: 'Manolo' },
-      children: [
-        { id: 'iMarta',
-          gen: 'nets',
-          img: 'http://usuaris.tinet.cat/sag/imatges/altree/marta.png',
-          name: 'Marta',
-          spouse: { id: 'iMiguelMarta', name: 'Miguel' },
+        img: 'http://usuaris.tinet.cat/sag/imatges/altree/herminia.png',
+        name: 'Herminia',
+        spouse: { 
+          id: 'iManolo',
+          gen: 'fills',
+          img: 'http://usuaris.tinet.cat/sag/imatges/altree/manolo.png',
+          name: 'Manolo' },
           children: [
-          { id: 'iJulia',
-            gen: 'besnets',
-            img: 'http://usuaris.tinet.cat/sag/imatges/altree/julia.png',
-            name: 'Julia' } ]
-        },
-        { id: 'iManuelMarta', name: 'Manuel', 
-          children: [
+            { id: 'iMarta',
+              gen: 'nets',
+              img: 'http://usuaris.tinet.cat/sag/imatges/altree/marta.png',
+              name: 'Marta',
+              spouse: { id: 'iMiguelMarta', name: 'Miguel' },
+              children: [
+                { id: 'iJulia',
+                  gen: 'besnets',
+                  img: 'http://usuaris.tinet.cat/sag/imatges/altree/julia.png',
+                  name: 'Julia' },
+		        { id: 'iManuelMarta', name: 'Manuel' }
+		      ]
+            },
             { id: 'iJuanma',
               gen: 'nets',
               img: 'http://usuaris.tinet.cat/sag/imatges/altree/juanma.png',
-              name: 'JuanMa' },
-            { id: 'iLourdes',
-              gen: 'nets',
-              img: 'http://usuaris.tinet.cat/sag/imatges/altree/lourdes.png',
-              name: 'Lourdes' },
-            { id: 'iMario', name: 'Mario' },
-            { id: 'iPablo', name: 'Pablo' },
-            { id: 'iVictor', name: 'Victor' },
+              name: 'JuanMa',
+	  	    spouse: { 
+	  		  id: 'iLourdes',
+                gen: 'nets',
+                img: 'http://usuaris.tinet.cat/sag/imatges/altree/lourdes.png',
+                name: 'Lourdes' },
+		      children: [
+                { id: 'iMario', name: 'Mario' },
+                { id: 'iPablo', name: 'Pablo' },
+                { id: 'iVictor', name: 'Victor' },
+  	          ]
+  	        },
             { id: 'iSebita',
-              gen: 'nets',
-              img: 'http://usuaris.tinet.cat/sag/imatges/altree/sebita.png',
-              name: 'Sebita (V)' },
-            { id: 'iRosaSebita',
-              gen: 'nets',
-              img: 'http://usuaris.tinet.cat/sag/imatges/altree/rosa_sebita.png',
-              name: 'Rosa' }
-        ]}
-    ]},
-    { id: 'iNane',
-      gen: 'fills',
-      img: 'http://usuaris.tinet.cat/sag/imatges/altree/nane.png',
-      name: 'Nane (IV)' },
-    { id: 'iAnaNane',
-      gen: 'fills',
-      img: 'http://usuaris.tinet.cat/sag/imatges/altree/ana_nane.png',
-      name: 'Sebas (VI)' },
-    { id: 'iPilar', name: 'Pilar', 
-      spouse: { 
-        id: 'iJuanPilar',
-        gen: 'fills',
-        img: 'http://usuaris.tinet.cat/sag/imatges/altree/juan_pilar.png',
-        name: 'Juan' },
-      children: [
-        { id: 'iGabriela',
-          gen: 'nets',
-          img: 'http://usuaris.tinet.cat/sag/imatges/altree/gabriela.png',
-          name: 'Gabriela' },
-        { id: 'iJuanito', name: 'Juanito' }
-    ]},
-    { id: 'iMontseSevilla',
-      gen: 'fills',
-      img: 'http://usuaris.tinet.cat/sag/imatges/altree/montse_sevilla.png',
-      name: 'Montse',
-      spouse:  { 
-        id: 'iManuelMontse',
-        gen: 'fills',
-        img: 'http://usuaris.tinet.cat/sag/imatges/altree/manuel.png',
-        name: 'Manuel' },
-      children: [
-        { id: 'iMariaMontse',
-          gen: 'nets',
-          img: 'http://usuaris.tinet.cat/sag/imatges/altree/maria.png',
-          name: 'Maria' }
-    ]} 
-  ]},
-  { id: 'iMontseCofita',
-    gen: 'root',
-    img: 'http://usuaris.tinet.cat/sag/imatges/altree/montse_cofita.png',
-    name: 'Montse (1933)',
-    spouse:  { 
-      id: 'iJosere',
-      gen: 'root',
-      img: 'http://usuaris.tinet.cat/sag/imatges/altree/josere.png',
-      name: 'Joseré' },
-    children: [
-      { id: 'iPepe', name: 'Pepe' },
-      { id: 'iConchi',
-        gen: 'fills',
-        img: 'http://usuaris.tinet.cat/sag/imatges/altree/conchi.png',
-        name: 'Conchi' },
-      { id: 'iJavierConchi', name: 'Javier' },
-      { id: 'iJaviConchi', name: 'Javi' },
-      { id: 'iNuria', name: 'Nuria' }
-  ]},
-  { id: 'iAurelio',
-    gen: 'root',
-    img: 'http://usuaris.tinet.cat/sag/imatges/altree/aurelio_400px.png',
-    name: 'Aurelio (1935)',
-    spouse: { id: 'iHerminiaAu', name: 'Herminia' },
-    children: [
-      { id: 'iJosan',
-        gen: 'fills',
-        img: 'http://usuaris.tinet.cat/sag/imatges/altree/josan.png',
-        name: 'Josan',
-        spouse:  { 
-          id: 'iJudit',
-          gen: 'fills',
-          img: 'http://usuaris.tinet.cat/sag/imatges/altree/judit.png',
-          name: 'Judit' },
-        children: [
-          { id: 'iJavierJosan',
-            gen: 'nets',
-            img: 'http://usuaris.tinet.cat/sag/imatges/altree/javier_josan.png',
-            name: 'Javier' },
-          { id: 'iMartin',
-            gen: 'nets',
-            img: 'http://usuaris.tinet.cat/sag/imatges/altree/martin.png',
-            name: 'Martin' }
+                gen: 'nets',
+                img: 'http://usuaris.tinet.cat/sag/imatges/altree/sebita.png',
+                name: 'Sebita (V)',
+                spouse: { id: 'iRosaSebita',
+                gen: 'nets',
+                img: 'http://usuaris.tinet.cat/sag/imatges/altree/rosa_sebita.png',
+                name: 'Rosa' }
+            }
       ]},
-      { id: 'iEva',
+      { id: 'iNane',
         gen: 'fills',
-        img: 'http://usuaris.tinet.cat/sag/imatges/altree/jose_eva_400px.jpg',
-        name: 'Eva',
-        spouse: { id: 'iJoseFco',
+        img: 'http://usuaris.tinet.cat/sag/imatges/altree/nane.png',
+        name: 'Nane (IV)' ,
+        spouse: { id: 'iAnaNane',
           gen: 'fills',
-          img: 'http://usuaris.tinet.cat/sag/imatges/altree/jose_fr_400px.png',
-          name: 'Jose Francisco' },
+          img: 'http://usuaris.tinet.cat/sag/imatges/altree/ana_nane.png',  
+	      name: 'AnaNane' },
+        children: [ { id: 'iSebasNanename', name: 'Sebas (VI)' } ]
+   	  },
+      { id: 'iPilar', name: 'Pilar', 
+        spouse: { 
+          id: 'iJuanPilar',
+          gen: 'fills',
+          img: 'http://usuaris.tinet.cat/sag/imatges/altree/juan_pilar.png',
+          name: 'Juan' },
         children: [
-          { id: 'iJorge', name: 'Jorge' },
-          { id: 'iMartita',
+          { id: 'iGabriela',
             gen: 'nets',
-            img: 'http://usuaris.tinet.cat/sag/imatges/altree/martita.png',
-            name: 'Martita' }
-      ]}
-  ]},
-  { id: 'iHerminia', name: 'Herminia (1935)',
-    spouse: { id: 'iMaqui', name: 'Antonio' },
-    children: [
-      { id: 'iConsuelo',
+            img: 'http://usuaris.tinet.cat/sag/imatges/altree/gabriela.png',
+            name: 'Gabriela' },
+          { id: 'iJuanito', name: 'Juanito' }
+      ]},
+      { id: 'iMontseSevilla',
         gen: 'fills',
-        img: 'http://usuaris.tinet.cat/sag/imatges/altree/consuelo_400px.jpg',
-        name: 'Consuelo',
-        spouse:
-        { id: 'iVictorConsuelo',
+        img: 'http://usuaris.tinet.cat/sag/imatges/altree/montse_sevilla.png',
+        name: 'Montse',
+        spouse:  { 
+          id: 'iManuelMontse',
           gen: 'fills',
-          img: 'http://usuaris.tinet.cat/sag/imatges/altree/victor.png',
-          name: 'Victor' },
+          img: 'http://usuaris.tinet.cat/sag/imatges/altree/manuel.png',
+          name: 'Manuel' },
         children: [
-          { id: 'iAlvaro', name: 'Alvaro' },
-          { id: 'iFernando', name: 'Fernando' },
-          { id: 'iAna', name: 'Ana' },
-          { id: 'iTelma', name: 'Telma' },
-      ]}
-  ]},
-  { id: 'iMaCarmen', name: 'Ma Carmen (1938)',
-    spouse: { id: 'iMiguel', name: 'Miguel' },
-    children: [
-      { id: 'iIrene', name: 'Irene' },
-      { id: 'iPaulino', name: 'Paulino' },
-      { id: 'iElena', name: 'Elena' },
-      { id: 'iGemma', name: 'Gemma' },
-      { id: 'iMiguelAngel', name: 'Miguel Angel' },
-      { id: 'iFerran', name: 'Ferran' },
-      { id: 'iJoel', name: 'Joel' } 
-    ]
-   }
+          { id: 'iMariaMontse',
+            gen: 'nets',
+            img: 'http://usuaris.tinet.cat/sag/imatges/altree/maria.png',
+            name: 'Maria' }
+      ]} 
+    ]},
+    { id: 'iMontseCofita',
+      gen: 'root',
+      img: 'http://usuaris.tinet.cat/sag/imatges/altree/montse_cofita.png',
+      name: 'Montse (1933)',
+      spouse: { 
+        id: 'iJosere',
+        gen: 'root',
+        img: 'http://usuaris.tinet.cat/sag/imatges/altree/josere.png',
+        name: 'Joseré' },
+      children: [  
+        { id: 'iPepe', name: 'Pepe' },
+        { id: 'iConchi',
+          gen: 'fills',
+          img: 'http://usuaris.tinet.cat/sag/imatges/altree/conchi.png',
+          name: 'Conchi',
+	  	spouse: { id: 'iJavierConchi', name: 'Javier' },
+	      children: [
+            { id: 'iJaviConchi', name: 'Javi' },
+            { id: 'iNuria', name: 'Nuria' }
+          ]
+        }
+    ]},
+    { id: 'iAurelio',
+      gen: 'root',
+      img: 'http://usuaris.tinet.cat/sag/imatges/altree/aurelio_400px.png',
+      name: 'Aurelio (1935)',
+      spouse: { id: 'iHerminiaAu', name: 'Herminia' },
+      children: [
+        { id: 'iJosan',
+          gen: 'fills',
+          img: 'http://usuaris.tinet.cat/sag/imatges/altree/josan.png',
+          name: 'Josan',
+          spouse:  { 
+            id: 'iJudit',
+            gen: 'fills',
+            img: 'http://usuaris.tinet.cat/sag/imatges/altree/judit.png',
+            name: 'Judit' },
+          children: [
+            { id: 'iJavierJosan',
+              gen: 'nets',
+              img: 'http://usuaris.tinet.cat/sag/imatges/altree/javier_josan.png',
+              name: 'Javier' },
+            { id: 'iMartin',
+              gen: 'nets',
+              img: 'http://usuaris.tinet.cat/sag/imatges/altree/martin.png',
+              name: 'Martin' }
+          ]},
+        { id: 'iEva',
+          gen: 'fills',
+          img: 'http://usuaris.tinet.cat/sag/imatges/altree/jose_eva_400px.jpg',
+          name: 'Eva',
+          spouse: { id: 'iJoseFco',
+            gen: 'fills',
+            img: 'http://usuaris.tinet.cat/sag/imatges/altree/jose_fr_400px.png',
+            name: 'Jose Francisco' },
+          children: [
+            { id: 'iJorge', name: 'Jorge' },
+            { id: 'iMartita',
+              gen: 'nets',
+              img: 'http://usuaris.tinet.cat/sag/imatges/altree/martita.png',
+              name: 'Martita' }
+          ]
+		}
+      ]
+	},
+    { id: 'iHerminia', name: 'Herminia (1935)',
+      gen: 'root',
+      spouse: { id: 'iMaqui', name: 'Antonio' },
+      children: [
+        { id: 'iConsuelo',
+          gen: 'fills',
+          img: 'http://usuaris.tinet.cat/sag/imatges/altree/consuelo_400px.jpg',
+          name: 'Consuelo',
+          spouse:
+          { id: 'iVictorConsuelo',
+            gen: 'fills',
+            img: 'http://usuaris.tinet.cat/sag/imatges/altree/victor.png',
+            name: 'Victor' },
+          children: [
+            { id: 'iAlvaro', name: 'Alvaro' }
+          ]
+      },
+	  { id: 'iFernando', name: 'Fernando',
+	    spouse: { id: 'iAna', name: 'Ana' },
+		children: [
+		  { id: 'iTelma', name: 'Telma' }
+		] },
+      ]
+    },
+    { id: 'iMaCarmen', name: 'Ma Carmen (1938)',
+      gen: 'root',
+      spouse: { id: 'iMiguel', name: 'Miguel' },
+      children: [
+        { id: 'iIrene', name: 'Irene',
+	      spouse: { id: 'iPaulino', name: 'Paulino' },
+	      children: [
+		    { id: 'iElena', name: 'Elena' }
+          ]},
+        { id: 'iGemma', name: 'Gemma',
+          children: [
+		    { id: 'iPablo', name: 'Pablo' } 
+	      ]},
+        { id: 'iMiguelAngel', name: 'Miguel Angel',
+	      spouse: { id: 'iSandra', name: 'Sandra' },
+		  children: [
+            { id: 'iFerran', name: 'Ferran' },
+            { id: 'iJoel', name: 'Joel' } 
+	      ]}
+      ]
+    }
  ];
-
-
